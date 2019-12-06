00e4a3d12e04d998fe98047d75ded966df048dde
Marcelo <marcelobbff@gmail.com>
2019-12-05 23:55:01 -0300

    app now uses main function

--- unit_test.py
+++ unit_test.py
@@ -1,1 +1,10 @@
 import app
+from player import Paddle
+
+
+def test_player_start_score():
+    paddle = Paddle('player 1')
+    paddle2 = Paddle('player 2')
+    assert paddle.score == 0
+    assert paddle2.score == 0
+
